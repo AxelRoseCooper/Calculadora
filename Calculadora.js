2673cb1f1dde34619498b5f51e1f767747155ec7
Axel Mercado <joaxel.mercado@gmail.com>
2023-01-14 13:19:54 -0600

    Calculadora

---
+++ Calculadora.js
@@ -0,0 +1,13 @@
+class Calculadora {
+    sumar = (num1, num2) =>  num1 + num2;
+    
+
+    restar = (num1, num2) => num1 - num2;
+    
+
+    dividir = (num1, num2) => num1 / num2;
+    
+
+    multiplicar = (num1, num2) =>  num1 * num2;
+    
+} 
